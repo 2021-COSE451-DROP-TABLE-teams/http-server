221e29ccd386b46fa2d06b541384601d89ebed8c
Lanph3re <lanph3re@gmail.com>
2021-05-06 01:06:37 +0900

    ADD debug code in guestbook.c

--- guestbook.c
+++ guestbook.c
@@ -4,6 +4,8 @@
 #include <time.h>
 #include <unistd.h>
 
+#define DEBUG
+
 #define TRUE 1
 #define FALSE 0
 
@@ -135,10 +137,16 @@ int update_tsv() {
     p_message = strstr(buffer, "comment=");
     token = strtok(p_message, "=");
     message = strtok(NULL, "&");
+#ifdef DEBUG
+    fprintf(stderr, "[DEBUG] message: %s\n", message);
+#endif
   } else {
     p_id = strstr(buffer, "post-id=");
     token = strtok(p_id, "=");
     id = strtok(NULL, "&");
+#ifdef DEBUG
+    fprintf(stderr, "[DEBUG] post-id: %s\n", id);
+#endif
   }
 
   char* p_hash = strstr(buffer, "password=");
@@ -149,6 +157,11 @@ int update_tsv() {
   token = strtok(p_author, "=");
   char* author = strtok(NULL, "&");
   free(buffer);
+
+#ifdef DEBUG
+    fprintf(stderr, "[DEBUG] hash: %s\n", hash);
+    fprintf(stderr, "[DEBUG] author: %s\n", author);
+#endif
 
   if (do_create) {  // create operation
     FILE* fp = fopen(filename, "a+");
